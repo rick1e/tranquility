9940e9b75d4d1358e002d03ad56660c4a1faeaf7
alrick-bcsl <abrown@bluechipstrategies.com.jm>
2022-10-29 13:23:40 -0500

    Updates

---
+++ precache-manifest.c8b43073bdd915415a8b42b400f8fd89.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = [
+  {
+    "revision": "00f5199532adcd58b293",
+    "url": "/tranquility/static/css/main.85f10bb6.chunk.css"
+  },
+  {
+    "revision": "00f5199532adcd58b293",
+    "url": "/tranquility/static/js/main.00f51995.chunk.js"
+  },
+  {
+    "revision": "3da4e6a8535dd6d8027b",
+    "url": "/tranquility/static/js/1.3da4e6a8.chunk.js"
+  },
+  {
+    "revision": "f6453a21e6e5bd897471",
+    "url": "/tranquility/static/js/2.f6453a21.chunk.js"
+  },
+  {
+    "revision": "b38afea1c3b95a97d544",
+    "url": "/tranquility/static/js/runtime~main.b38afea1.js"
+  },
+  {
+    "revision": "1851428a922d5074b3beb020da02b351",
+    "url": "/tranquility/static/media/cropped-lotus.1851428a.svg"
+  },
+  {
+    "revision": "bb7a7b6c10fecf4815a6bd68822173b5",
+    "url": "/tranquility/static/media/undraw_snap_the_moment_re_88cu.bb7a7b6c.svg"
+  },
+  {
+    "revision": "51d689b52a852741a99e50cddcdf68b9",
+    "url": "/tranquility/static/media/camp-canoe.51d689b5.mp4"
+  },
+  {
+    "revision": "13683d9ab6964103f8cee8e0e550bc4d",
+    "url": "/tranquility/static/media/camp-fire.13683d9a.mp4"
+  },
+  {
+    "revision": "61c3c65959c34bf72139a72e262b9fed",
+    "url": "/tranquility/static/media/camp-rain.61c3c659.mp4"
+  },
+  {
+    "revision": "7877feb8f7ef709760d80c5c6be70579",
+    "url": "/tranquility/static/media/hike-river.7877feb8.mp4"
+  },
+  {
+    "revision": "06fecd379b8943f4106f4f7ea7ddbd16",
+    "url": "/tranquility/static/media/sea-day.06fecd37.mp4"
+  },
+  {
+    "revision": "63a3d5c71df31b8898f4f934f2fb7870",
+    "url": "/tranquility/static/media/sea-dusk.63a3d5c7.mp4"
+  },
+  {
+    "revision": "75881a60c10c54cd0c93a76197b7015b",
+    "url": "/tranquility/static/media/sea-rain.75881a60.mp4"
+  },
+  {
+    "revision": "b7361b2692492f2a2f929a7291f4f842",
+    "url": "/tranquility/static/media/stream.b7361b26.mp4"
+  },
+  {
+    "revision": "cdceda4e0b1b2e0a8758b504e5138862",
+    "url": "/tranquility/index.html"
+  }
+];
